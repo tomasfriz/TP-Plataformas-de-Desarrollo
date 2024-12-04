8fa347e9d2627b84a8b8e79ba593f86b8635b767
TomasPisarski <tomas.gabriel.sosa99@gmail.com>
2024-12-04 20:07:38 -0300

    back eventos y usuarios

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,20 @@
+import admin from 'firebase-admin';
+
+export async function createEvent(eventData) {
+  const docRef = admin.firestore().collection('events').doc();
+  await docRef.set(eventData);
+  return docRef.id;
+}
+
+export async function updateEvent(eventId, updatedData) {
+  await admin.firestore().collection('events').doc(eventId).update(updatedData);
+}
+
+export async function getEvent(eventId) {
+  const doc = await admin.firestore().collection('events').doc(eventId).get();
+  return doc.exists ? doc.data() : null;
+}
+
+export async function deleteEvent(eventId) {
+  await admin.firestore().collection('events').doc(eventId).delete();
+}
---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+import pkg from 'firebase-admin';
+const { auth } = pkg;
+import bcrypt from 'bcrypt'
+
+export async function register({email, password, rol}) {
+
+  const userRecord = await auth().createUser({ email, password });
+
+  await auth().setCustomUserClaims(userRecord.uid, { rol });
+  return userRecord;
+}
+
+export async function login({email, password}) {
+  const user = await auth().getUserByEmail(email);
+  return user;
+  
+}
