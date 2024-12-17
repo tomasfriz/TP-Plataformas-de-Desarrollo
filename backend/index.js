e3b39b41b5bb297d3b10adcdf3313dea33c2e706
TomasPisarski <tomas.gabriel.sosa99@gmail.com>
2024-12-17 17:51:35 -0300

    fix serviceaccountkey

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import indexRouter from "./routes/index.js"
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };;
+import fs from 'fs/promises';
+const serviceAccountKey = JSON.parse(await fs.readFile('./serviceAccountKey.json', 'utf8'));
 import cors from 'cors'
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountKey),
 });
 
 const app = express();
